e453c6f969599d138185d0f064a4dfc47e1185b2
EC2 Default User <ec2-user@ip-172-31-0-141.eu-west-1.compute.internal>
2018-10-25 08:19:25 +0000

    featur blocking card added

--- creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -1,11 +1,24 @@
 package pl.jkan.creditcard;
 
 class CreditCard {
-    public void assignLimit(double money) {
-        
-    }
+    
+     
+     public boolean blockade = false;
+     
+     public void assignLimi(double limit){
+         
+     }
+    
     
     public double getLimit() {
         return 2000;
+    }
+    
+    public void block(){
+        this.blockade=true;
+    }
+    
+    public boolean isBloked{
+        return this.blockade;
     }
 }
