ea39d8837929e650568f2e0933fbf99b05c8ab3a
Dominik Malcharczyk <dominik.malcharczyk@wp.pl>
2018-11-14 22:17:07 +0100

    add withdraw feature

---
+++ creditcard-app/src/main/java/pl/dmal/creditcard/CreditCard.java
@@ -0,0 +1,40 @@
+package pl.dmal.creditcard;
+import java.math.BigDecimal;
+
+class CreditCard {
+
+    private BigDecimal limit;
+     
+     public boolean blockade = false;
+     private BigDecimal balance = BigDecimal.ZERO;
+
+     public void assignLimit(BigDecimal money){
+        this.limit = money;
+        this.balance = money;
+     }
+    
+    
+    public BigDecimal getLimit() {
+        return limit;
+    }
+    
+    public void block(){
+        this.blockade=true;
+    }
+    
+    public boolean isBlocked(){
+        return this.blockade;
+    }
+
+    public void withdraw(BigDecimal money) {
+
+        if (isNotEnoughMoney(money))
+        throw new WithdrawWhenOverTheLimit();
+
+        balance = balance.subtract(money);
+    }
+
+    private boolean isNotEnoughMoney(BigDecimal money) {
+        return money.compareTo(balance) > 0;
+    }
+}
---
+++ creditcard-app/src/test/java/pl/dmal/creditcard/CreditCardTest.java
@@ -0,0 +1,46 @@
+package pl.dmal.creditcard;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class CreditCardTest {
+
+    private static BigDecimal money(double x) {
+        return BigDecimal.valueOf(x);
+    }
+
+    @Test
+    public void allowAssignLimitToCard() {
+        CreditCard card1 = new CreditCard();
+
+        card1.assignLimit(money(2000));
+
+        Assert.assertTrue(card1.getLimit().equals(money(2000)));
+    }
+
+    @Test(expected = WithdrawWhenOverTheLimit.class)
+    public void cantWithdrawWhenCantAfford() {
+        CreditCard card = new CreditCard();
+        card.assignLimit(money(2000));
+        card.withdraw(money(3000));
+        Assert.assertTrue(card.getLimit().equals(money(2000)));
+    }
+    
+    @Test
+    public void cardBlockCard(){
+        CreditCard card = new CreditCard();
+        card.block();
+    
+        Assert.assertTrue(card.isBlocked());
+    }
+
+    @Test(expected = WithdrawWhenOverTheLimit.class)
+    public void cantWithdrawWhenOverTheLimit() throws Exception{
+        CreditCard card = new CreditCard();
+        card.assignLimit(money(200));
+        card.withdraw(money(300));
+    }
+
+}
